e8687e2a9ff2273ba7df671eb204fd8480231bd1
DenizWassmuth <deniz-wassmuth@gmx.de>
2025-12-19 10:17:30 +0100

    update addOrderTest name

--- src/test/java/ShopServiceTest.java
+++ src/test/java/ShopServiceTest.java
@@ -24,7 +24,7 @@ class ShopServiceTest {
     }
 
     @Test
-    void addOrderTest_whenInvalidProductId_expectNull() {
+    void addOrderTest_whenInvalidProductId_throwsProductDoesNotExistException() {
         //GIVEN
         ShopService shopService = new ShopService();
         List<String> productsIds = List.of("1", "2");
